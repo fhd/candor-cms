88b3c69a115a4d0d81935e24d64c045e02134b79
Felix H. Dahlke <fhd@ubercode.de>
2010-10-14 13:33:04 +0200

    Refactoring.

--- src/main/clojure/com/github/fhd/candorcms/core.clj
+++ src/main/clojure/com/github/fhd/candorcms/core.clj
@@ -1,8 +1,8 @@
 (ns com.github.fhd.candorcms.core
   "The core service of Candor CMS, responsible for retrieving the pages."
   (:use compojure.core
-	ring.util.servlet
-	hiccup.core)
+        ring.util.servlet
+        hiccup.core)
   (:gen-class
    :extends javax.servlet.http.HttpServlet))
 
@@ -15,46 +15,48 @@
   [file]
   (doto (java.util.Properties.)
     (.load (-> (Thread/currentThread)
-	       (.getContextClassLoader)
-	       (.getResourceAsStream file)))))
+               (.getContextClassLoader)
+               (.getResourceAsStream file)))))
 
 (defn parse-header
   "Parses the page header format into a map."
   [header]
   (into {} (for [line (.split header "\n")]
-	     (if (not (empty? line))
-	       (let [key-value (.split line "=")]
-		 [(keyword (aget key-value 0))
-		  (aget key-value 1)])))))
+             (if (not (empty? line))
+               (let [key-value (.split line "=")
+                     key (aget key-value 0)
+                     value (aget key-value 1)]
+                 [(keyword key) value])))))
 
 (defn load-pages
   "Loads all available pages."
   [site-dir]
   (let [pages-dir (str site-dir "/pages")]
     (into {} (for [file (.listFiles (java.io.File. pages-dir))]
-	       (let [name (.getName file)
-		     simple-name (.substring name 0 (.indexOf name "."))
-		     content (slurp (str pages-dir "/" name))
-		     header-end (.indexOf content "-->")
-		     header (parse-header (.substring content 4 header-end))
-		     body (.trim (.substring content (+ header-end 3)))]
-		 [(keyword simple-name)
-		  (struct page
-			  (str "/" (if (not (= simple-name index-page-name))
-				     simple-name))
-			  (:title header)
-			  (:template header)
-			  body)])))))
+               (let [name (.getName file)
+                     simple-name (.substring name 0 (.indexOf name "."))
+                     url (str "/" (if (not (= simple-name index-page-name))
+                                    simple-name))
+                     content (slurp (str pages-dir "/" name))
+                     header-end (.indexOf content "-->")
+                     header (parse-header (.substring content 4 header-end))
+                     body (.trim (.substring content (+ header-end 3)))]
+                 [(keyword simple-name)
+                  (struct page
+                          url
+                          (:title header)
+                          (:template header)
+                          body)])))))
 
 (defn load-templates
   "Loads all available templates."
   [site-dir]
   (let [templates-dir (str site-dir "/templates")]
     (into {} (for [file (.listFiles (java.io.File. templates-dir))]
-	       (let [name (.getName file)
-		     simple-name (.substring name 0 (.indexOf name "."))
-		     content (slurp (str templates-dir "/" name))]
-		 [(keyword simple-name) content])))))
+               (let [name (.getName file)
+                     simple-name (.substring name 0 (.indexOf name "."))
+                     content (slurp (str templates-dir "/" name))]
+                 [(keyword simple-name) content])))))
 
 (defn expand-template
   "Expands (i.e. applies) the template of the supplied page."
@@ -65,21 +67,21 @@
   "Returns the contents of the supplied page."
   [page-name]
   (let [site-dir (.getProperty (load-properties "candorcms.properties")
-			       "site.dir")]
+                               "site.dir")]
     (if (empty? site-dir)
       (html [:html
-	     [:head [:title "Candor CMS - No site configured"]]
-	     [:body
-	      [:p "No site has been configured for Candor CMS."]
-	      [:p "Please edit the file " [:em "candorcms.properties"]
-	       " inside the web application archive."]]])
+             [:head [:title "Candor CMS - No site configured"]]
+             [:body
+              [:p "No site has been configured for Candor CMS."]
+              [:p "Please edit the file " [:em "candorcms.properties"]
+               " inside the web application archive."]]])
       (let [pages (load-pages site-dir)
-	    page ((keyword page-name) pages)
-	    templates (load-templates site-dir)]
-	(expand-template (:content ((keyword (:template page)) templates))
-			 {:title (:title page)
-			  :pages pages
-			  :content (:content page)})))))
+            page ((keyword page-name) pages)
+            templates (load-templates site-dir)]
+        (expand-template (:content ((keyword (:template page)) templates))
+                         {:title (:title page)
+                          :pages pages
+                          :content (:content page)})))))
 
 (defroutes all
   (GET "/" [] (get-page index-page-name))
