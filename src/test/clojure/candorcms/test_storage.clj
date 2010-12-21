38c1bab5dd3cd5740d01dd4ba0a3f1c70d738721
Felix H. Dahlke <fhd@ubercode.de>
2010-12-21 21:17:10 +0100

    Added more storage test cases.

--- src/test/clojure/candorcms/test_storage.clj
+++ src/test/clojure/candorcms/test_storage.clj
@@ -1,11 +1,19 @@
 (ns candorcms.test-storage
   (:use clojure.test
         candorcms.storage)
-  (:import candorcms.storage.Page))
+  (:import (candorcms.storage Page Article)))
+
+(def simple-site-dir "src/test/resources/simple-site")
 
 (deftest test-load-pages
-  (is (= {:index (Page. "/index" "Index" "none" "<p>Hello, World</p>")}
-         (load-pages "src/test/resources/simple-site"))))
+  (is (= {:index (Page. "/index" "Index" "layout" "<p>Hello, World</p>")}
+         (load-pages simple-site-dir))))
 
-;; TODO: Write test cases for load-templates and load-articles
+(deftest test-load-templates
+  (is (= {:layout "<html><body>{{{content}}}</body></html>"}
+         (load-templates simple-site-dir))))
 
+(deftest test-load-articles
+  (is (= {:article1 (Article. "/index/article1" "Article 1" "2010-12-21"
+                              "<p>Article One.</p>")}
+         (load-articles simple-site-dir "index" "yyyy-MM-dd"))))
