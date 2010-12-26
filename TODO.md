400521cf2ab3668f44620ed4668c80210e2c4e57
Felix H. Dahlke <fhd@ubercode.de>
2010-12-26 02:41:00 +0100

    Added test cases for incomplete headers.

--- TODO.md
+++ TODO.md
@@ -1,5 +1,6 @@
 0.1
 ===
+* Read article dates from header instead of file system.
 * Implement Markdown support for articles (identify it based on the
   file extension like GitHub).
 * Add article pagination.
--- src/test/clojure/candorcms/test_storage.clj
+++ src/test/clojure/candorcms/test_storage.clj
@@ -4,7 +4,7 @@
   (:import (candorcms.storage Page Article)))
 
 (def simple-site-dir "src/test/resources/simple-site")
-(def no-headers-site-dir "src/test/resources/no-headers-site")
+(def header-test-site-dir "src/test/resources/header-test-site")
 (def date-format "yyyy-MM-dd")
 
 (deftest test-load-pages
@@ -16,15 +16,23 @@
          (load-templates simple-site-dir))))
 
 (deftest test-load-articles
-  (is (= {:article1 (Article. "/index/article1" "Article 1" "2010-12-21"
+  (is (= {:article1 (Article. "/index/article1" "Article 1" "2010-12-26"
                               "<p>Article One.</p>")}
          (load-articles simple-site-dir "index" date-format))))
 
 (deftest test-load-pages-without-header
-  (is (= {:index (Page. "/index" nil nil "<p>Hello, World</p>")}
-         (load-pages no-headers-site-dir))))
+  (is (= (Page. "/no-header" nil nil "Test")
+         (:no-header (load-pages header-test-site-dir))))) 
+
+(deftest test-load-pages-without-title
+  (is (= (Page. "/no-title" nil "layout" "Test")
+         (:no-title (load-pages header-test-site-dir)))))
+
+(deftest test-load-pages-without-template
+  (is (= (Page. "/no-template" "No Template" nil "Test")
+         (:no-template (load-pages header-test-site-dir)))))
 
 (deftest test-load-articles-without-header
-  (is (= {:article1 (Article. "/index/article1" nil "2010-12-21"
-                              "<p>Article One.</p>")}
-         (load-articles no-headers-site-dir "index" date-format))))
+  (is (= (Article. "/index/no-header" nil "2010-12-26" "Test")
+         (:no-header (load-articles header-test-site-dir "index"
+                      date-format)))))
